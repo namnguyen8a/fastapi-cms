7dbc086e0c78e69f6c6c24a6eb10bf8b03f37c15
namnguyen <namnguyen.8a@gmail.com>
2025-05-28 11:38:32 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Core CMS features:
+- *User Authentication and Authorization*
+- *Content Modeling*
+- *CRUD Operations for Content*
+- *File Upload*
+- *A Rich Text Editor*
+- *API Endpoints*
+- *Admin Interface*
+
+# Refs:
+- https://drive.google.com/drive/folders/1ixp17p0PIQTV4ZK80Df8wkWaA6fNPPch?fbclid=IwY2xjawKi2U9leHRuA2FlbQIxMABicmlkETFvSFM2RXRsSlhKTHcycHUwAR45gwVsYlkNqNyb0NH3ZkQD8iXNhJcgsOAC09kKwWz1LPJdPBbEpNjazAvJqg_aem_Ee06b_QNqvZc7zZ2GwICsg
---
+++ features/authen-author/test.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "fakehashedsecret",
+        "disabled": False,
+    },
+    "alice": {
+        "username": "alice",
+        "full_name": "Alice",
+        "email": "alice@example.com",
+        "hashed_password": "fakehashedsecret2",
+        "disabled": True,
+    },
+}
+
+
+class User(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
+
+class UserInDB(User):
+    hashed_password: str
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict), type(UserInDB(**user_dict))
+
+print(get_user(fake_users_db, "alice"))
