84c588d771b397505c50e5a4a45881fe79513e7f
Nam Nguyen <namnguyen.8a@gmail.com>
2025-06-09 08:51:33 +0700

    adding authen and author technique

--- examples/fastapi-jwt/security_jwt.py
+++ examples/fastapi-jwt/security_jwt.py
@@ -3,7 +3,7 @@
 from typing import Annotated
 from fastapi import FastAPI, Depends
 from pydantic import BaseModel
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 import jwt
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
@@ -22,6 +22,13 @@ def create_access_token(data):
     encode_jwt = jwt.encode(data_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encode_jwt
 
+def verity_token(token):
+    payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+    username = payload.get("sub")
+    if username is None:
+        return {"msg": "Could not validate"}
+    return username
+
 class User(BaseModel):
     username: str
     password: str
@@ -37,17 +44,20 @@ async def create_user(user: User):
     return {"msg": "created success!"}
 
 @app.post("/token")
-async def login(user: User):
-    username = user.username
-    password = user.password
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    username = form_data.username
+    password = form_data.password
     if username not in db:
         return {"msg": "incorrect username or password"}
-    if password != db[username]["password"]:
+    if hashed_password(password) != db[username]["password"]:
         return {"msg": "incorrect username or password"}
     jwt_token = create_access_token({"sub": username})
 
-    return {"jwt_token": jwt_token}
+    return {"access_token": jwt_token, "token_type": "bearer"}
 
 @app.get("/get-users")
-async def get_user():
+async def get_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    username = verity_token(token)
+    if not username:
+        return {"msg": "Invalid token"}
     return db
